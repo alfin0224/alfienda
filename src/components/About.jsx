56f3dc1db9f29f47d8dc9e62e349603dd010b0d8
alfin0224 <alfinmuhamad0224@gmail.com>
2023-08-02 19:34:31 +0700

    Update Home and About me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,26 +15,17 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-6">
-          &nbsp; &nbsp; &nbsp; I was born in Banyumas, Central Java, Indonesia on February 24, 1997.
-            The first time I started to focus on learning information technology was when I started school at SMK TELKOM Purwokerto, and I graduated from school after studying for 3 years.
-            After I graduated from Vocational High School, I still focused on learning IT by continuing my education at the Indonesian Islamic University majoring in informatics engineering 
-            and I graduated from college after studying for 4 years.
+          &nbsp; &nbsp; &nbsp; I was born in Banyumas, Central Java, Indonesia, on February 24, 1997. The first time I started to focus on learning information technology was when I began school at SMK TELKOM Purwokerto. I graduated from the school after studying there for 3 years. Following my graduation from Vocational High School, I continued my education at the Indonesian Islamic University, majoring in informatics engineering, and graduated from college after studying for 4 years.
         </p>
         <br />
 
         <p className="text-xl">
-        &nbsp; &nbsp; &nbsp;I have work experience for 2 years and 7 months at KOMINFO West Papua Province as a web developer. 
-        After that I worked remotely as a web developer for 1 year at DPMK West Papua Province. Then, in early 2023 to May 2023 I helped develop 
-        web applications that are used as advertising media, manage membership data and program data for learning activities in the sefeo english community, 
-        as a Fullstack Developer
+        &nbsp; &nbsp; &nbsp; I have 2 years and 7 months of work experience at KOMINFO West Papua Province as a web developer. After that, I worked remotely as a web developer for 1 year at DPMK West Papua Province. From early 2023 to June 2023, I helped develop web applications used for advertising media, managing membership data, and programming data for learning activities in the Sefeo English community as a Fullstack Developer.
           </p>
         <br />
 
         <p className="text-xl">
-        &nbsp; &nbsp; &nbsp; I have a passion for building beautiful, modern websites to meet needs and solve problems in business processes. 
-          A forward-thinking and curiosity-driven web developer who offers more than 4 years experience in web development by writing clean, 
-          efficient, reusable and reliable code for various business processes such as Pertamina RU IV Cilacap, Halfway House for children with
-          Cancer and Blood Disorders, various provincial government systems, and Sefeo English Community.
+        &nbsp; &nbsp; &nbsp; I have a passion for building beautiful, modern websites to meet needs and solve problems in business processes. I am a forward-thinking and curiosity-driven web developer, offering more than 4 years of experience in web development by writing clean, efficient, reusable, and reliable code for various business processes. Some of my notable projects include Pertamina RU IV Cilacap, Halfway House for Children with Cancer and Blood Disorders, various provincial government systems, and the Sefeo English Community.
         </p>
       </div>
     </div>
